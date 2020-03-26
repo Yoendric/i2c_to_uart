d1fd7a517aebaaf9576cb247ba973b40f96ec26f
Yoendric Oropesa Mendoza <yoendricoropesa@gmail.com>
2020-03-26 12:06:54 -0600

    Address GPIO

--- i2c/i2c.ino
+++ i2c/i2c.ino
@@ -3,13 +3,31 @@
 
 volatile byte RecvArray[8];
 volatile byte PzemRead[25];
+volatile int  Address[4];
+int address = 10;
+int GPIO1 = 48;
+int GPIO2 = 49;
+int GPIO3 = 50;
+int GPIO4 = 51;
 
 void setup()
 {
- Wire.begin(10);                // join i2c bus with address #2
+ Serial.begin(9600);           // start serial for output
+ pinMode(GPIO1, INPUT_PULLUP);
+ pinMode(GPIO2, INPUT_PULLUP);
+ pinMode(GPIO3, INPUT_PULLUP);
+ pinMode(GPIO4, INPUT_PULLUP);
+ delay(1);
+ Address[0]=digitalRead(GPIO1);
+ Address[1]=digitalRead(GPIO2);
+ Address[2]=digitalRead(GPIO3);
+ Address[3]=digitalRead(GPIO4);
+ for(int i=0;i<4;i++){
+   address+=Address[i]*pow(2,i);
+ }
+ Wire.begin(int(address));                // join i2c bus with address #2
  Wire.onReceive(recvEvent); // register event
  Wire.onRequest(requestEvent);
- Serial.begin(9600);           // start serial for output
  Serial1.begin(9600);
 }
 
